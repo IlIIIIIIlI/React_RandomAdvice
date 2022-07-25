36add6e0c1ed79c6d9dc3391204e2c385eb3e823
iliiiiiili <yangquechen@gmail.com>
2022-07-26 00:36:43 +1000

    add button

--- src/App.js
+++ src/App.js
@@ -53,7 +53,10 @@ class App extends React.Component {
             <div className="app">
                 <div className="card">
                     <h1 className="heading">{advice}</h1>
-                    </div>
+                    <button className='button'>\
+                        <span>GIVE me Some Advice!</span>
+                    </button>
+                </div>
             </div>
         );
     }
