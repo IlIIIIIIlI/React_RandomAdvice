40ca9b874d1047e4793d0ef347422b679ae783ec
iliiiiiili <yangquechen@gmail.com>
2022-07-26 00:12:14 +1000

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React from 'react';
+// we use axios to extract apis, and we need to install the thing in the cmd
+// npm install --save axios
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    // let's see the app's component
+    state = {
+        // ONLY one field
+        advice: ''
+    };
+
+    // where we call the API
+    componentDidMount() {
+        // for testing
+        // console.log('Component did mount')
+        this.fetchAdvice();
+    }
+
+    // this function is a method, means it is belongs to this class
+    fetchAdvice = () => {
+        axios.get('https://api.adviceslip.com/advice')
+            // get the response
+            .then((response) => {
+                // we use this to get data steps by steps, 一步步更深入
+                // console.log(response.data);
+                // or 
+                const { advice } = response.data.slip;
+                // after we get the data, we update the value to state
+                this.setState({ advice: advice });
+            })
+
+            // and catch the error
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }
+
+
+    render() {
+
+        // get it out of the state
+        const { advice } = this.state;
+
+        return (
+            <h1>{advice}</h1>
+        );
+    }
 }
 
 export default App;
+
+
+
